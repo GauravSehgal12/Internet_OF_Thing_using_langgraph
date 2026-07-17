709f8290d4904b7d2e35167a63c078eadf76931c
GauravSehgal12 <b22me020@iitj.ac.in>
2026-07-17 21:27:26 +0530

    groq llm integration done properly

---
+++ get_llm.py
@@ -0,0 +1,33 @@
+import os
+
+from langchain_groq import ChatGroq
+
+# from langchain_groq import ChatGroq
+def get_llm(
+    model="llama-3.3-70b-versatile",
+    temperature=0.7,
+):
+    if not os.getenv("GROQ_API_KEY"):
+        raise ValueError(
+            "GROQ_API_KEY not found. Add it to your .env file."
+        )
+
+    try:
+        llm = ChatGroq(
+            model=model,
+            temperature=temperature,
+            max_retries=2,
+        )
+
+        llm.invoke("Hi")
+
+        print(f"--- Using Groq ({model}) ---")
+        return llm
+
+    except Exception as e:
+        raise RuntimeError(f"Failed to connect to Groq: {e}")
+
+
+if __name__ == "__main__":
+    llm = get_llm()
+    print(llm.invoke("what is the name of the first president of the united states?"))
